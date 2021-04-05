a6974adc61b303e13f697e80a371daf1e8f6f220
msfukui <msfukui@gmail.com>
2021-04-05 11:33:02 +0900

    Impl InsertionSort

---
+++ 2_sort/4_insertion_sort/insertion.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// InsertionSort は
+// 与えられた整数のスライスから、
+// ソートの結果と挿入ソートで並び替えた結果の整数のスライスを返します。
+// c.f. No.2-4 挿入ソート
+func InsertionSort(list []int) (success bool, sortedList []int) {
+	// 返却値用のバッファを作ってコピー
+	sortedList = make([]int, len(list))
+	copy(sortedList, list)
+
+	buf := 0 // 入れ替え用のバッファ
+
+	fmt.Printf("input  = %v\n", list)
+
+	for i, _ := range sortedList {
+		fmt.Printf("  list(%v) = %v\n", i, sortedList)
+		for j := i - 1; j >= 0; j-- {
+			fmt.Printf("    list(%v,%v) = %v\n", i, j, sortedList)
+			if sortedList[j] > sortedList[j+1] {
+				buf = sortedList[j]
+				sortedList[j] = sortedList[j+1]
+				sortedList[j+1] = buf
+			} else {
+				break
+			}
+		}
+	}
+
+	fmt.Printf("output = %v\n", sortedList)
+
+	return true, sortedList
+}
---
+++ 2_sort/4_insertion_sort/insertion_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestInsertionSortEmpty(t *testing.T) {
+	const success = true
+	input := []int{}
+	expected := []int{}
+
+	if r, actual := InsertionSort(input); r != success || !reflect.DeepEqual(actual, expected) {
+		t.Errorf("InsertionSort(%v) = %v, %v, want %v, %v", input, r, actual, success, expected)
+	}
+}
+
+func TestInsertionSortSuccess(t *testing.T) {
+	const success = true
+	input := []int{5, 3, 4, 7, 2, 8, 6, 9, 1}
+	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	if r, actual := InsertionSort(input); r != success || !reflect.DeepEqual(actual, expected) {
+		t.Errorf("InsertionSort(%v) = %v, %v, want %v, %v", input, r, actual, success, expected)
+	}
+}
+
+func TestInsertionSortDuplexSuccess(t *testing.T) {
+	const success = true
+	input := []int{5, 3, 4, 7, 2, 5, 8, 6, 9, 1}
+	expected := []int{1, 2, 3, 4, 5, 5, 6, 7, 8, 9}
+
+	if r, actual := InsertionSort(input); r != success || !reflect.DeepEqual(actual, expected) {
+		t.Errorf("InsertionSort(%v) = %v, %v, want %v, %v", input, r, actual, success, expected)
+	}
+}
